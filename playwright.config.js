20f3d661368c452f1f3339b91868240779b6de31
podolyak-tatyana <podolyak.tatyana2309@gmail.com>
2026-02-01 17:09:38 +0200

    Implement tests for Users

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+
   use: {
     baseURL: "http://localhost:5173",
-    headless: true,
+
+    headless: false, // показывать браузер
+    viewport: null,  // убрать фиксированный размер
+
+    launchOptions: {
+      args: ["--start-maximized"], //  открыть максимально
+      slowMo: 700,                 // замедление (можно 200/500)
+    },
   },
+
   webServer: {
     command: "npm run dev -- --host --port 5173",
     url: "http://localhost:5173",
